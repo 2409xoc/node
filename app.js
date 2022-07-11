36983deeeb88aa68e85129ac3f978c74a41214a5
2409hw <2409hw@gmail.com>
2022-07-11 11:55:05 -0400

    rewrite make.js

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
+var express = require("express");
 var fs = require("fs");
 var fse = require("fs-extra");
 var path = require("path");
+
 var ejs = require("ejs");
-var fmatter = require("front-matter");
 var marked = require("marked");
 var count = require("html-word-count");
+var fmatter = require("front-matter");
+
 const config = require("./config.js");
 const ffs = require('fast-folder-size')
-const { create } = require("domain");
-const { getStatusMapping } = require("cucumber/lib/status.js");
-const headers = `${__dirname}/views/partials/header.html`;
-var all, dirlist, blog, sci, tags, wait=300;
 
+const { getStatusMapping } = require("cucumber/lib/status.js");
+const { create } = require("domain");
+var headers = `${__dirname}/${config.dev.headers.split("./")[1]}`;
+var all, dirlist, blog, sci, tags, wait=200;
+
+var app = express(), hostname="http://127.0.0.1", port=3000;
+app.engine("html", require("ejs").renderFile);
+app.use(express.static(path.join(__dirname, "public")));
 
 function read() {
 	return new Promise((resolve, reject) => {
@@ -109,6 +116,7 @@ function split() {
 		setTimeout(resolve, wait);
 	})
 }
+
 
 function delBuild() {
     return new Promise((resolve, reject) => {
@@ -273,20 +281,109 @@ function cleanUp(dirs) {
 	})
 }
 
-async function make() {
-    await read();
-    await bubblesort();
+async function basic() {
+	await read();
+	await bubblesort();
 	await addMarked();
 	await getTags();
 	await split();
-    await delBuild();
-    await createBuild();
-	await createSubs();
-    await subpages();
-	await createBlogs();
-	await writePosts(); 
-	await cleanUp(await scanDirs(config.dev.builddir));
-
+	var args = process.argv.slice(2);
+	if (args[0] && args[0] == "make") {
+		await delBuild();
+		await createBuild();
+		await createSubs();
+		await subpages();
+		await createBlogs();
+		await writePosts(); 
+		await cleanUp(await scanDirs(config.dev.builddir));
+	}
+	else {
+		app.listen(port, () => { console.log(`Running: ${hostname}:${port}`)});
+	}
 }
 
-make();
+
+
+app.use("/blog/*", (req, res) => {
+	if (req.baseUrl == "/blog") {
+		res.render("pages/blog.html", {blog:blog, headers: headers, tags: tags});
+	}
+	else {
+		var template;
+		var relative = `.${req.baseUrl}.md`;
+		if (fs.existsSync(relative)) {
+			fs.readFile(relative, "utf8", function(err, data) {
+				if (err) {
+					throw err;
+				}
+				var content = fmatter(data);
+				var body = marked.parse(content.body);
+				if (content.attributes.layout && (fs.existsSync(`views/blog/${content.attributes.layout}.html`))) {
+					template = `blog/${content.attributes.layout}.html`;
+				}
+				else {
+					template = `blog/post.html`;
+				}
+				res.render(template, {post: content, body: body, count: count(body), headers:headers, tags: tags});
+			})
+		}
+		else {
+			res.render("partials/404.html", {link: req.baseUrl, headers:headers});
+		}
+	}
+})
+
+app.use("/sci/*", (req, res) => {
+	if (req.baseUrl == "/sci") {
+		res.render("pages/sci.html", {sci: sci, headers:headers})
+	}
+	else {
+		var template;
+		var relative = `.${req.baseUrl}.md`;
+		if (fs.existsSync(relative)) {
+			fs.readFile(relative, "utf8", function(err, data) {
+				if (err) {
+					throw err;
+				}
+				var content = fmatter(data);
+				var body = marked.parse(content.body);
+				res.render("blog/sci.html", {post: content, body: body, headers:headers});
+			})
+		}
+		else {
+			res.render("partials/404.html", {link: req.baseUrl, headers:headers});
+		}
+	}
+})
+
+app.use("*", (req, res) => {
+	var url, metadata_dict={}, args;
+	if (req.baseUrl == "") {
+		url = req.baseUrl;
+	}
+	else {
+		url = req.baseUrl.split("/")[1];
+	}
+	var config_exist = config.build[url] !== undefined;
+	if (config_exist && (fs.existsSync(`${__dirname}${config.build[url][0]}`))) {
+		if (config.build[url].length >= 2) {
+			args = config.build[url][1];
+			var i;
+			for (i=0; i<args.length; ++i) {
+				metadata_dict[args[i]] = eval(args[i]);
+			}
+		}
+		else {
+			metadata_dict = {};
+		}
+		if (url == "") {
+			url = "index";
+		}
+		res.render(`pages/${url}.html`, metadata_dict);
+	}
+	else {
+		res.render("partials/404.html", {link: req.baseUrl, headers: headers});
+	}
+})
+
+basic();
